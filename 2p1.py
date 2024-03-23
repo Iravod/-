0b4a78ee1f67784224c754e3c535677534183f59
Iravod <161827319+Iravod@users.noreply.github.com>
2024-03-23 13:12:55 +0300

    Add files via upload

---
+++ 2p1.py
@@ -0,0 +1,14 @@
+x=int(input("Введите x:"))
+if -20<=x<=-10:
+  print("Входит")
+else:
+    print("Не входит")
+
+x=int(input("Введите x:"))
+if -3<x<=-1:
+    print("Входит")
+else:
+    if 0<=x<=15:
+        print("Входит")
+    else:
+      print("Не входит")
